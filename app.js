c0484fceefcc8e7dfb39a7b061b016b99887b4b8
Tim <tim_hanson85@live.com>
2015-07-08 11:32:11 -0700

    modified app.js file to use directions

--- app.js
+++ app.js
@@ -1,10 +1,53 @@
+var rendererOptions = {
+  draggable: true
+};
+var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
+var directionsService = new google.maps.DirectionsService();
+var map;
+
+var oregon = new google.maps.LatLng(40.0000, -120.5000);
+
 function initialize() {
-       var mapCanvas = document.getElementById('map-canvas');
-       var mapOptions = {
-         center: new google.maps.LatLng(45.5200, -122.6819),
-         zoom: 8,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
-       }
-       var map = new google.maps.Map(mapCanvas, mapOptions)
-     }
-     google.maps.event.addDomListener(window, 'load', initialize);
+
+  var mapOptions = {
+    zoom: 7,
+    center: oregon
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directionsPanel'));
+
+  google.maps.event.addListener(directionsDisplay, 'directions_changed', function() {
+    computeTotalDistance(directionsDisplay.getDirections());
+  });
+
+  calcRoute();
+}
+
+function calcRoute() {
+
+  var request = {
+    origin: 'portland,Or',
+    destination: 'portland,Or',
+    waypoints:[{location: 'salem,or'}, {location: 'dallas,or'}],
+    travelMode: google.maps.TravelMode.DRIVING,
+    optimizeWaypoints: true
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+function computeTotalDistance(result) {
+  var total = 0;
+  var myroute = result.routes[0];
+  for (var i = 0; i < myroute.legs.length; i++) {
+    total += myroute.legs[i].distance.value;
+  }
+  total = total / 1000.0;
+  document.getElementById('total').innerHTML = total + ' km';
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

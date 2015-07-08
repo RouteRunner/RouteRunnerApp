338ffa1970436d4ffc77254510c82d7a5f814b1c
Tim <tim_hanson85@live.com>
2015-07-08 14:50:52 -0700

    working with app.js. added a geocoder to look up locations

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ var rendererOptions = {
 var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var geocoder;
 
-var oregon = new google.maps.LatLng(40.0000, -120.5000);
+
+// var oregon = new google.maps.LatLng(40.0000, -120.5000);
 
 function initialize() {
 
   var mapOptions = {
-    zoom: 7,
-    center: oregon
+    zoom: 8,
+    // center: portland,or
   };
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   directionsDisplay.setMap(map);
@@ -29,7 +31,7 @@ function calcRoute() {
   var request = {
     origin: 'portland,Or',
     destination: 'portland,Or',
-    waypoints:[{location: 'salem,or'}, {location: 'dallas,or'}],
+    waypoints:[{location: 'wal-mart salem,or'}, {location: 'dallas,or'}],
     travelMode: google.maps.TravelMode.DRIVING,
     optimizeWaypoints: true
   };
@@ -48,6 +50,22 @@ function computeTotalDistance(result) {
   }
   total = total / 1000.0;
   document.getElementById('total').innerHTML = total + ' km';
+}
+
+//new geocode thingy
+function codeAddress() {
+ var address = document.getElementById('address').value;
+ geocoder.geocode( { 'address': address}, function(results, status) {
+   if (status == google.maps.GeocoderStatus.OK) {
+     map.setCenter(results[0].geometry.location);
+     var marker = new google.maps.Marker({
+         map: map,
+         position: results[0].geometry.location
+     });
+   } else {
+     alert('Geocode was not successful for the following reason: ' + status);
+   }
+ });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);

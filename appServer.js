3d317d4f3a7d62db9bfed0162fb33d6431297b4a
sjellis <stephenjohnellis@gmail.com>
2015-07-14 15:23:40 -0700

    more config

--- appServer.js
+++ appServer.js
@@ -5,10 +5,9 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var router = express.Router();
 var logger = require('morgan');
-var uuid = require('node-uuid');
+var uuid = require('uuid');
 var nodemailer = require('nodemailer');
 if (!process.env.heroku) var configs = require('./js/config.js');
-var uuid = require('node-uuid');
 
 var app = express();
 module.exports = app;

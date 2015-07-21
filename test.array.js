5bec7cb79208dc212433aae9fd0db80bcc75a6a7
sjellis <stephenjohnellis@gmail.com>
2015-07-20 21:17:04 -0700

    setup framework for running tests in the browser

--- test.array.js
+++ test.array.js
@@ -1,25 +1,23 @@
-var assert = require('assert');
-
 var fakeArray = [];
 
-describe('My fake array object', function() {
-  describe('The pop method', function() {
+// describe('My fake array object', function() {
+//   describe('The pop method', function() {
+//
+//     before(function() {
+//       fakeArray[0] = 1;
+//       fakeArray[1] = 2;
+//       fakeArray[2] = 3;
+//       fakeArray.length = 3;
+//     });
+//
+//     it('should return the final element', function() {
+//       chai.expect(fakeArray.pop()).to.eq(3);
+//     });
+//   });
+// });
 
-    before(function() {
-      fakeArray[0] = 1;
-      fakeArray[1] = 2;
-      fakeArray[2] = 3;
-      fakeArray.length = 3;
-    });
-
-    it('should return the final element', function() {
-      assert.equal(fakeArray.pop(), 3);
-    });
-  });
-});
-
-describe('Set Origin Button', function(){
-  it('should bring up modal', function(){
-    expect.
-  })
-})
+// describe('Set Origin Button', function(){
+//   it('should bring up modal', function(){
+//     expect.
+//   })
+// })
